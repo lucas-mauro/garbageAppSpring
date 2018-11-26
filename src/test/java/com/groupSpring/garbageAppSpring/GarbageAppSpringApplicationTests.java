8764376685b6001a5706cbb881cbd94ed40b0135
lucas-mauro <lucasmaurogil@gmail.com>
2018-11-26 11:39:43 -0300

    Initial commit

---
+++ src/test/java/com/groupSpring/garbageAppSpring/GarbageAppSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.groupSpring.garbageAppSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GarbageAppSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
